d02f4ea37d14bcde8f2e84c924ce551f941653f0
sIri <bljuzgavic.22@gmail.com>
2016-12-07 07:21:04 +0100

    Cleaned up and added primary keys

--- database/migrations/2016_12_07_051134_create_rented_by.php
+++ database/migrations/2016_12_07_051134_create_rented_by.php
@@ -14,10 +14,16 @@ class CreateRentedBy extends Migration
     {
         Schema::create('rented_by', function (Blueprint $table) {
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->integer('car_id')->unsigned();
-            $table->foreign('car_id')->references('id')->on('cars')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('car_id')->references('id')->on('cars')
+                ->onUpdate('cascade')->onDelete('cascade');
+                
             $table->timestamps();
+
+            $table->primary(['user_id', 'car_id']);
         });
     }
 
